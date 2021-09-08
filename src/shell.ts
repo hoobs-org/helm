167e0a08c708db03434a51c025053f580fc9869f
mkellsy <mkellsy@gmail.com>
2021-09-07 23:34:22 -0600

    license header

--- src/shell.ts
+++ src/shell.ts
@@ -1,5 +1,5 @@
 /**************************************************************************************************
- * hoobsd                                                                                         *
+ * helm                                                                                           *
  * Copyright (C) 2020 HOOBS                                                                       *
  *                                                                                                *
  * This program is free software: you can redistribute it and/or modify                           *
